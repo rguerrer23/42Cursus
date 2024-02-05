be49bec0ad10f6cecc204f291f1b13073af119e9
rguerrer23 <ricardoguerrerocabrera@gmail.com>
2024-02-05 16:14:59 +0100

    ...

--- so_long/src/draw_map.c
+++ so_long/src/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: rguerrer <rguerrer@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 15:49:16 by rguerrer          #+#    #+#             */
-/*   Updated: 2024/02/05 16:00:19 by rguerrer         ###   ########.fr       */
+/*   Updated: 2024/02/05 16:14:48 by rguerrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,14 @@ void	draw_player(t_game *game, int x, int y)
 void	draw_exit(t_game *game, int x, int y)
 {
 	if (game->map[y][x] == 'E')
-		mlx_image_to_window(game->mlx->mlx, game->img->img_exit, x * 32, y * 32);
+		mlx_image_to_window(game->mlx->mlx, game->img->img_exit, x * 32, y
+			* 32);
 	else
 	{
-		mlx_image_to_window(game->mlx->mlx, game->img->img_exit, x * 32, y * 32);
-		mlx_image_to_window(game->mlx->mlx, game->img->img_player, x * 32, y * 32);
+		mlx_image_to_window(game->mlx->mlx, game->img->img_exit, x * 32, y
+			* 32);
+		mlx_image_to_window(game->mlx->mlx, game->img->img_player, x * 32, y
+			* 32);
 	}
 }
 
