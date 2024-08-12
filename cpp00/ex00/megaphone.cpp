12efdb35d183ee17e6a1071e7d4f681fdca96411
rguerrer23 <ricardoguerrerocabrera@gmail.com>
2024-08-12 18:03:01 +0200

    pending norm

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rguerrer <rguerrer@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 17:38:40 by rguerrer          #+#    #+#             */
+/*   Updated: 2024/08/12 17:38:40 by rguerrer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				std::cout << (char)std::toupper(argv[i][j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << '\n';
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	return 0;
+}
