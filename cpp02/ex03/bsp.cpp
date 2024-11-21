24f0b8bf1f5e80ffad7dc3be8ca2da27a0afa2d9
rguerrer23 <ricardoguerrerocabrera@gmail.com>
2024-11-21 17:00:51 +0100

    ..

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -1,3 +1,5 @@
 #include "Point.hpp"
 
-bool bsp(P)
+bool bsp(Point const a, Point const b, Point const c, Point const point) {
+    return (b - a).crossProduct(point - a) >= 0 && (c - b).crossProduct(point - b) >= 0 && (a - c).crossProduct(point - c) >= 0;
+}
