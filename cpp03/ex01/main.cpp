3de48fa4619aed25214340fcd87752c0f68b8f44
rguerrer23 <ricardoguerrerocabrera@gmail.com>
2024-11-27 18:55:45 +0100

    ..

---
+++ cpp03/ex01/main.cpp
@@ -0,0 +1,24 @@
+#include "ClapTrap.hpp"
+
+int main()
+{
+	std::cout << "### TESTING CLAPTRAP ###\n" << std::endl;
+	{
+		std::cout << "\033[34mConstructing\033[0m" << std::endl;
+		ClapTrap a;
+		ClapTrap b("Cody");
+
+		std::cout << "\033[34mTesting\033[0m" << std::endl;
+		a.attack("some other robot");
+		a.takeDamage(10);
+		a.takeDamage(10);
+		a.beRepaired(5);
+		a.attack("some other other robot");
+		b.beRepaired(3);
+		for (int i = 0; i < 12; i++)
+			b.attack("Cody-clone");
+		b.beRepaired(3);
+		std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
+	}
+	return (0);
+}
